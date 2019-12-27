855e27dcde27c44f4e049e3befdde5f219e6c0ae
Aleksey Gagarkin <gagarkin@virtualhealth.com>
2019-12-27 14:57:53 +0700

    Tarantool 2.3 + vinyl

--- app/insert.php
+++ app/insert.php
@@ -14,7 +14,7 @@ $time_start = microtime(true);
 
 for ($ruleId=1 ; $ruleId < 100; $ruleId ++) {
     for ($triggerId=1 ; $triggerId < 100; $triggerId ++) {
-        for ($answerSetId=1 ; $answerSetId < 10; $answerSetId ++) {
+        for ($answerSetId=1 ; $answerSetId < 100; $answerSetId ++) {
             $insert = "INSERT INTO snapshot_verification_result VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
             $result = $client->executeUpdate($insert, $ruleId, $triggerId, $answerSetId, mt_rand(1, 50), mt_rand(1, 30000), mt_rand(0,1), md5(mt_rand()), date("Y-m-d H:i:s"));
             $count++;
